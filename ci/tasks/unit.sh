8815d7697512718eb3ae1b5f6e64ce852418f8c2
making <makingx@gmail.com>
2016-05-03 19:28:45 +0900

    Add Config Client

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+cd config-server-repo
+./mvnw spring-boot:run -Dmaven.repo.local=../m2/rootfs/opt/m2 -Drun.arguments="--server.port=8888" &
+
+while true; do
+    check=`curl localhost:8888`
+    if [ "X$check" != "X" ]; then
+        break
+    fi
+    echo "wait for config server boot"
+    sleep 1
+done
+cd ..
 
 cd repo
 ./mvnw test -Dmaven.repo.local=../m2/rootfs/opt/m2
