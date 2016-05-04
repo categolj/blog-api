3ab3517f3a3fd26ad383add3f7473078a30a2d42
making <makingx@gmail.com>
2016-05-04 22:53:15 +0900

    Fix packaging

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,5 +2,6 @@
 
 cd repo
 ./mvnw package -Dmaven.test.skip=true -Dmaven.repo.local=../m2/rootfs/opt/m2
+./mvnw help:evaluate -Dexpression=project.artifactId
 artifactId=`./mvnw help:evaluate -Dexpression=project.artifactId | egrep -v '(^\[INFO])'`
 cp target/${artifactId}.jar ../output/app.jar
