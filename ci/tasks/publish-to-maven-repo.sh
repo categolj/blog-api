66e21ecf68378b8a687dfcaf479728cca0847946
making <makingx@gmail.com>
2016-05-05 00:35:23 +0900

    Modify publishing script

--- ci/tasks/publish-to-maven-repo.sh
+++ ci/tasks/publish-to-maven-repo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd repo
-./mvnw deploy --settings ci/tasks/settings.xml -Dmaven.test.skip=true -Dmaven.repo.local=../m2/rootfs/opt/m2
+./mvnw deploy --settings ci/tasks/settings.xml -Dmaven.test.skip=true -Dmaven.repo.local=../m2/rootfs/opt/m2 -DperformRelease=true
