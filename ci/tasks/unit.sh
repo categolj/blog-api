12fa14c8e9c62d6524d0cbac3b06427f9bdaba15
making <makingx@gmail.com>
2016-05-04 22:03:07 +0900

    Disable Config Server in testing

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -1,17 +1,4 @@
-#!/bin/bash
-
-cd config-server-repo
-./mvnw spring-boot:run -Dmaven.repo.local=../m2/rootfs/opt/m2 -Drun.arguments="--server.port=8888" &
-
-while true; do
-    check=`curl localhost:8888`
-    if [ "X$check" != "X" ]; then
-        break
-    fi
-    echo "wait for config server boot"
-    sleep 1
-done
-cd ..
+#!/bin/sh
 
 cd repo
 ./mvnw test -Dmaven.repo.local=../m2/rootfs/opt/m2
