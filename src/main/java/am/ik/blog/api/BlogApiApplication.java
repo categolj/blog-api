ecd924fa361105667254c26add7f9c9a6d26a91f
making <makingx@gmail.com>
2017-02-25 02:59:37 +0900

    Workarround

--- src/main/java/am/ik/blog/api/BlogApiApplication.java
+++ src/main/java/am/ik/blog/api/BlogApiApplication.java
@@ -2,9 +2,11 @@ package am.ik.blog.api;
 
 import am.ik.categolj3.api.EnableCategoLJ3ApiServer;
 import am.ik.categolj3.api.entry.redis.EntryRedisTemplateFactory;
+import am.ik.categolj3.api.git.GitProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -14,6 +16,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 @SpringBootApplication
 @EnableCategoLJ3ApiServer
 @EnableDiscoveryClient
+@EnableConfigurationProperties(GitProperties.class)
 public class BlogApiApplication {
 
     public static void main(String[] args) {
