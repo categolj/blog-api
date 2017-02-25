d6668c405d1f633bf6a43a7ddd0dc22390817c93
making <makingx@gmail.com>
2017-02-25 21:11:50 +0900

    Update pipeline

--- ci/tasks/publish-to-maven-repo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd repo
-./mvnw deploy --settings ci/tasks/settings.xml -Dmaven.test.skip=true -Dmaven.repo.local=../m2/rootfs/opt/m2 -DperformRelease=true
--- ci/tasks/unit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd repo
-./mvnw test -Dmaven.repo.local=../m2/rootfs/opt/m2
