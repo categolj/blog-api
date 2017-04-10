d59d1ae35f59e927e73852a8f81d50f9771d9760
making <makingx@gmail.com>
2017-04-11 00:18:06 +0900

    Add Tags API

---
+++ src/main/java/am/ik/blog/entry/TagController.java
@@ -0,0 +1,24 @@
+package am.ik.blog.entry;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(path = "api/tags")
+@RequiredArgsConstructor
+public class TagController {
+	private final TagMapper tagMapper;
+
+	@GetMapping
+	List<String> getTags() {
+		return tagMapper.findOrderByTagNameAsc().stream().map(Tag::getValue)
+				.collect(toList());
+	}
+}
---
+++ src/test/java/am/ik/blog/entry/TagControllerTest.java
@@ -0,0 +1,36 @@
+package am.ik.blog.entry;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.restassured.RestAssured;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql({ "classpath:/delete-test-data.sql", "classpath:/insert-test-data.sql" })
+public class TagControllerTest {
+	@LocalServerPort
+	int port;
+
+	@Before
+	public void setUp() throws Exception {
+		RestAssured.port = port;
+	}
+
+	@Test
+	public void getTags() throws Exception {
+		given().log().all().get("/api/tags").then().log().all().assertThat()
+				.statusCode(200).body("$", hasSize(3)).body("[0]", equalTo("test1"))
+				.body("[1]", equalTo("test2")).body("[2]", equalTo("test3"));
+	}
+
+}
