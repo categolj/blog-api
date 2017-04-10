0baf018a3a23d6b3cad30992c31224dc1b306778
making <makingx@gmail.com>
2017-04-11 02:17:35 +0900

    Add Spring Cloud Config, Spring Cloud Eureka

--- src/main/java/am/ik/blog/BlogApiApplication.java
+++ src/main/java/am/ik/blog/BlogApiApplication.java
@@ -2,8 +2,10 @@ package am.ik.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
