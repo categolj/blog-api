20b1cb36e010f2a94e46842243cd516ffa53a5fa
making <makingx@gmail.com>
2017-06-06 03:46:47 +0900

    Move CloudConfig

---
+++ src/main/java/am/ik/blog/config/CloudConfig.java
@@ -0,0 +1,23 @@
+package am.ik.blog.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig.ConnectionConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("cloud")
+public class CloudConfig extends AbstractCloudConfig {
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource.tomcat")
+	DataSource dataSource() {
+		return connectionFactory().dataSource(new DataSourceConfig(null,
+				new ConnectionConfig("allowMultiQueries=true")));
+	}
+}
