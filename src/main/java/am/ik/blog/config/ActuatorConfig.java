b293c0655cb80190f7f56238c28bd8764328f9c6
Toshiaki Maki <tmaki@pivotal.io>
2018-03-12 22:12:02 +0900

    Use default internal user authentication

--- src/main/java/am/ik/blog/config/ActuatorConfig.java
+++ src/main/java/am/ik/blog/config/ActuatorConfig.java
@@ -1,20 +1,14 @@
 package am.ik.blog.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
-@ConfigurationProperties("security")
 @Order(-5)
 public class ActuatorConfig extends WebSecurityConfigurerAdapter {
-	private User user = new User();
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.requestMatcher(EndpointRequest.toAnyEndpoint()) //
@@ -22,46 +16,5 @@ public class ActuatorConfig extends WebSecurityConfigurerAdapter {
 				.requestMatchers(EndpointRequest.to("health", "info")).permitAll() //
 				.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR") //
 				.and().httpBasic();
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		UserDetails details = org.springframework.security.core.userdetails.User
-				.withDefaultPasswordEncoder() //
-				.username(this.user.name) //
-				.password(this.user.password) //
-				.roles("ACTUATOR") //
-				.build();
-		auth.inMemoryAuthentication() //
-				.withUser(details);
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public static class User {
-		private String name = "user";
-		private String password = "password";
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
 	}
 }
---
+++ src/test/java/am/ik/blog/entry/ActuatorTest.java
@@ -0,0 +1,97 @@
+package am.ik.blog.entry;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.Base64Utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ActuatorTest {
+	@LocalServerPort
+	int port;
+	WebTestClient webClient;
+
+	@Before
+	public void setup() {
+		this.webClient = WebTestClient.bindToServer() //
+				.baseUrl("http://localhost:" + port) //
+				.build();
+	}
+
+	@Test
+	public void testInfo() {
+		this.webClient.get() //
+				.uri("/actuator/info") //
+				.exchange() //
+				.expectStatus().isOk().expectBody(JsonNode.class);
+	}
+
+	@Test
+	public void testHealthWithoutAuth() {
+		this.webClient.get() //
+				.uri("/actuator/health") //
+				.exchange() //
+				.expectStatus().isOk() //
+				.expectBody(JsonNode.class) //
+				.consumeWith(n -> {
+					JsonNode body = n.getResponseBody();
+					assertThat(body).isNotNull();
+					assertThat(body.has("status")).isTrue();
+					assertThat(body.get("status").asText()).isEqualTo("UP");
+					assertThat(body.has("details")).isFalse();
+				});
+	}
+
+	@Test
+	public void testHealthWithAuth() {
+		this.webClient.get() //
+				.uri("/actuator/health") //
+				.header("Authorization",
+						"Basic " + Base64Utils.encodeToString("test:pass".getBytes()))
+				.exchange() //
+				.expectStatus().isOk() //
+				.expectBody(JsonNode.class) //
+				.consumeWith(n -> {
+					JsonNode body = n.getResponseBody();
+					assertThat(body).isNotNull();
+					assertThat(body.has("status")).isTrue();
+					assertThat(body.get("status").asText()).isEqualTo("UP");
+					assertThat(body.has("details")).isTrue();
+				});
+	}
+
+	@Test
+	public void testPrometheusWithoutAuth() {
+		this.webClient.get() //
+				.uri("/actuator/prometheus") //
+				.exchange() //
+				.expectStatus().isUnauthorized() //
+				.expectBody(String.class) //
+				.consumeWith(n -> {
+					System.out.println(n.getResponseBody());
+				});
+	}
+
+	@Test
+	public void testPrometheusWithAuth() {
+		this.webClient.get() //
+				.uri("/actuator/prometheus") //
+				.header("Authorization",
+						"Basic " + Base64Utils.encodeToString("test:pass".getBytes()))
+				.exchange() //
+				.expectStatus().isOk() //
+				.expectBody(String.class) //
+				.consumeWith(n -> {
+					System.out.println(n.getResponseBody());
+				});
+	}
+}
