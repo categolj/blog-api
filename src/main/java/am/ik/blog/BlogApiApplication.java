4636b3ca2d89982db6a54b2fde8e6349889545ef
Toshiaki Maki <tmaki@pivotal.io>
2018-08-01 01:47:56 +0900

    Disable caching

--- src/main/java/am/ik/blog/BlogApiApplication.java
+++ src/main/java/am/ik/blog/BlogApiApplication.java
@@ -6,14 +6,12 @@ import am.ik.github.GitHubClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableAsync
-@EnableCaching
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/am/ik/blog/reactive/EntryHelper.java
+++ src/main/java/am/ik/blog/reactive/EntryHelper.java
@@ -4,7 +4,6 @@ import am.ik.blog.entry.Entry;
 import am.ik.blog.entry.EntryId;
 import am.ik.blog.entry.EntryMapper;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.cloud.sleuth.annotation.SpanTag;
 import org.springframework.stereotype.Component;
@@ -18,7 +17,6 @@ public class EntryHelper {
 	}
 
 	@NewSpan
-	@Cacheable("entry")
 	public Entry getEntry(@SpanTag("entryId") EntryId entryId,
 			@SpanTag("excludeContent") boolean excludeContent) {
 		return this.entryMapper.findOne(entryId, excludeContent);
