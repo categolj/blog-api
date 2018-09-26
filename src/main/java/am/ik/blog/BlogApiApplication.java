0db642e4f2dd3541e4a5af49b2585cf15293f2a7
Toshiaki Maki <tmaki@pivotal.io>
2018-09-26 02:12:18 -0400

    Bump to Spring Boot 2.1

--- src/main/java/am/ik/blog/BlogApiApplication.java
+++ src/main/java/am/ik/blog/BlogApiApplication.java
@@ -3,10 +3,10 @@ package am.ik.blog;
 import am.ik.blog.github.GitHubProps;
 import am.ik.github.AccessToken;
 import am.ik.github.GitHubClient;
-import com.zaxxer.hikari.HikariDataSource;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -28,11 +28,12 @@ public class BlogApiApplication {
 	}
 
 	@Bean
-	public Scheduler jdbcScheduler(HikariDataSource dataSource) {
+	public Scheduler jdbcScheduler(
+			@Value("${spring.datasource.hikari.maximum-pool-size}") int maxPoolSize) {
 		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
 		taskExecutor.setCorePoolSize(8);
 		taskExecutor.setQueueCapacity(0);
-		taskExecutor.setMaxPoolSize(dataSource.getMaximumPoolSize());
+		taskExecutor.setMaxPoolSize(maxPoolSize);
 		taskExecutor.afterPropertiesSet();
 		return Schedulers.fromExecutor(taskExecutor);
 	}
