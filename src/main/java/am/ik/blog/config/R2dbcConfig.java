34c39ca84a708fc45d2698498f4454e8c7a41476
Toshiaki Maki <tmaki@pivotal.io>
2018-11-24 19:56:15 +0900

    Use datasource-proxy-r2dbc

--- src/main/java/am/ik/blog/config/R2dbcConfig.java
+++ src/main/java/am/ik/blog/config/R2dbcConfig.java
@@ -2,9 +2,11 @@ package am.ik.blog.config;
 
 import java.net.URI;
 
+import brave.Tracer;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import net.ttddyy.dsproxy.r2dbc.ProxyConnectionFactoryBuilder;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,15 +19,19 @@ public class R2dbcConfig {
 	public ConnectionFactory connectionFactory(
 			@Value("${spring.datasource.url}") String uriString,
 			@Value("${spring.datasource.username}") String username,
-			@Value("${spring.datasource.password}") String password) {
+			@Value("${spring.datasource.password}") String password, Tracer tracer) {
 		URI uri = URI.create(uriString.replace("jdbc:", ""));
-		return new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder() //
-				.host(uri.getHost()) //
-				.port(uri.getPort()) //
-				.username(username) //
-				.password(password) //
-				.database(uri.getPath().replace("/", "")) //
-				.build());
+		PostgresqlConnectionFactory connectionFactory = new PostgresqlConnectionFactory(
+				PostgresqlConnectionConfiguration.builder() //
+						.host(uri.getHost()) //
+						.port(uri.getPort()) //
+						.username(username) //
+						.password(password) //
+						.database(uri.getPath().replace("/", "")) //
+						.build());
+		return ProxyConnectionFactoryBuilder.create(connectionFactory) //
+				.listener(new TracingExecutionListener(tracer)) //
+				.build();
 	}
 
 	@Bean
