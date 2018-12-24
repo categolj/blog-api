3593f07054a7a79bac8f44d4a1b9c66baab3dda2
Toshiaki Maki <tmaki@pivotal.io>
2018-12-25 00:45:43 +0900

    From Bean Validation to YAVI

--- src/main/java/am/ik/blog/github/GitHubProps.java
+++ src/main/java/am/ik/blog/github/GitHubProps.java
@@ -1,18 +1,19 @@
 package am.ik.blog.github;
 
-import javax.validation.constraints.NotEmpty;
+import am.ik.yavi.constraint.base.ContainerConstraintBase;
+import am.ik.yavi.core.ConstraintViolations;
+import am.ik.yavi.core.Validator;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 
 @Component
 @ConfigurationProperties(prefix = "blog.github")
 @Validated
-public class GitHubProps {
-	@NotEmpty
+public class GitHubProps implements org.springframework.validation.Validator {
 	private String accessToken;
-	@NotEmpty
 	private String webhookSecret;
 
 	public String getAccessToken() {
@@ -29,5 +30,24 @@ public class GitHubProps {
 
 	public void setWebhookSecret(String webhookSecret) {
 		this.webhookSecret = webhookSecret;
+	}
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return clazz == GitHubProps.class;
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Validator<GitHubProps> validator = Validator.builder(GitHubProps.class)
+				.constraint(GitHubProps::getAccessToken, "accessToken",
+						ContainerConstraintBase::notEmpty)
+				.constraint(GitHubProps::getWebhookSecret, "webhookSecret",
+						ContainerConstraintBase::notEmpty)
+				.build();
+		ConstraintViolations validate = validator.validate((GitHubProps) target);
+		if (!validate.isValid()) {
+			validate.apply(errors::rejectValue);
+		}
 	}
 }
