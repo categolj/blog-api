34b4e27f420789b0cf0ae206783fec514fe73f2a
Toshiaki Maki <tmaki@pivotal.io>
2019-01-21 18:49:46 +0900

    Include b3 in error response

---
+++ src/main/java/am/ik/blog/exception/BlogErrorAttributes.java
@@ -0,0 +1,31 @@
+package am.ik.blog.exception;
+
+import java.util.Map;
+
+import brave.Span;
+import brave.Tracer;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+@Component
+public class BlogErrorAttributes extends DefaultErrorAttributes {
+	private final Tracer tracer;
+
+	public BlogErrorAttributes(ServerProperties props, Tracer tracer) {
+		super(props.getError().isIncludeException());
+		this.tracer = tracer;
+	}
+
+	@Override
+	public Map<String, Object> getErrorAttributes(ServerRequest request,
+			boolean includeStackTrace) {
+		Map<String, Object> attributes = super.getErrorAttributes(request,
+				includeStackTrace);
+		Span span = this.tracer.currentSpan();
+		attributes.put("b3", span == null ? null : span.context().toString());
+		return attributes;
+	}
+}
