742c0062fa34b1e8c11f839188f1e3be369809d5
Toshiaki Maki <tmaki@pivotal.io>
2019-10-03 14:06:00 +0900

    Add EventListener

--- src/main/java/am/ik/blog/config/Resilience4jConfig.java
+++ src/main/java/am/ik/blog/config/Resilience4jConfig.java
@@ -2,10 +2,14 @@ package am.ik.blog.config;
 
 import java.time.Duration;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.core.EventProcessor;
 import io.github.resilience4j.micrometer.tagged.TaggedCircuitBreakerMetrics;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +32,16 @@ public class Resilience4jConfig {
 
 	@Bean
 	public InitializingBean init(MeterRegistry meterRegistry, CircuitBreakerRegistry circuitBreakerRegistry) {
+		final Logger log = LoggerFactory.getLogger(Resilience4jConfig.class);
+		circuitBreakerRegistry.getEventPublisher().onEvent(event -> log.warn("Event({})", event));
+		for (CircuitBreaker circuitBreaker : circuitBreakerRegistry.getAllCircuitBreakers()) {
+			final CircuitBreaker.EventPublisher eventPublisher = circuitBreaker.getEventPublisher();
+			if (!((EventProcessor) eventPublisher).hasConsumers()) {
+				eventPublisher.onError(event -> log.error("[onError] {}", event));
+				eventPublisher.onReset(event -> log.info("[onReset] {}", event));
+				eventPublisher.onStateTransition(event -> log.info("[onStateTransition] {}", event));
+			}
+		}
 		return () -> TaggedCircuitBreakerMetrics.ofCircuitBreakerRegistry(circuitBreakerRegistry).bindTo(meterRegistry);
 	}
 }
