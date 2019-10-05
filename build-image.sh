2912934490f2072f508a27dd18dc50d03ddd4110
Toshiaki Maki <tmaki@pivotal.io>
2019-10-05 15:23:22 +0900

    update

--- build-image.sh
+++ build-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-./mvnw clean package
+./mvnw clean package -Drestdoc.scheme=https -Drestdoc.host=blog-api.ik.am -Drestdoc.port=443
 
 #VERSION=$(grep '<version>' pom.xml | head -n 1 | tail -n 1 | sed -e 's|<version>||g' -e 's|</version>||g' -e 's| ||g' |  tr -d '\t')
 VERSION=latest
