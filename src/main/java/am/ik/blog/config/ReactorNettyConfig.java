6e379a8ac997d7a2ecd47d64b1a18a8b087dcc71
Toshiaki Maki <tmaki@pivotal.io>
2019-10-09 15:17:15 -0500

    Enable reactor netty metrics on client

--- src/main/java/am/ik/blog/config/ReactorNettyConfig.java
+++ src/main/java/am/ik/blog/config/ReactorNettyConfig.java
@@ -1,14 +1,24 @@
 package am.ik.blog.config;
 
+import reactor.netty.http.client.HttpClient;
+
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ReactorNettyConfig {
 	@Bean
 	public WebServerFactoryCustomizer<NettyReactiveWebServerFactory> customizer() {
 		return factory -> factory.addServerCustomizers(builder -> builder.metrics(true));
+	}
+
+	@Bean
+	public WebClient.Builder webClientBuilder() {
+		return WebClient.builder().clientConnector(
+				new ReactorClientHttpConnector(HttpClient.create().tcpConfiguration(builder -> builder.metrics(true))));
 	}
 }
