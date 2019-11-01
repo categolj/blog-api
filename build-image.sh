8eb7f6e489c50ca4bde3875e8c28b4451dfaaa6f
Toshiaki Maki <tmaki@pivotal.io>
2019-11-01 14:24:49 +0900

    Update Builder

--- build-image.sh
+++ build-image.sh
@@ -8,9 +8,6 @@ VERSION=latest
 pack build making/blog-api:${VERSION} \
   -p target/blog-api-*.jar \
   --publish \
-  --no-pull \
-  --builder cloudfoundry/cnb:bionic \
-  --buildpack org.cloudfoundry.openjdk,org.cloudfoundry.jvmapplication,org.cloudfoundry.springboot,org.cloudfoundry.distzip
-  #--builder asia.gcr.io/fe-tmaki/cf-build-service-dev-219913-build-service-builders-p-builder-08a87070945c7177ae30c35d16d0f4ed \
-  #--buildpack org.cloudfoundry.archiveexpanding,io.pivotal.openjdk,org.cloudfoundry.jvmapplication,org.cloudfoundry.springboot,org.cloudfoundry.distzip
+  --builder making/java-cnb-builder \
+  #--builder cloudfoundry/cnb:bionic \
 
