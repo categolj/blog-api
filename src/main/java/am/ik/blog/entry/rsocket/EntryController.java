4399f01490f5a899c5e4725e360ae59cf037a2af
Toshiaki Maki <tmaki@pivotal.io>
2020-07-06 11:41:11 +0900

    Drop @ NewSpace from EntryController

--- src/main/java/am/ik/blog/entry/rsocket/EntryController.java
+++ src/main/java/am/ik/blog/entry/rsocket/EntryController.java
@@ -3,50 +3,46 @@ package am.ik.blog.entry.rsocket;
 import am.ik.blog.entry.Entry;
 import am.ik.blog.entry.EntryMapper;
 import io.rsocket.exceptions.ApplicationErrorException;
-import org.springframework.cloud.sleuth.annotation.NewSpan;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import org.springframework.cloud.sleuth.annotation.SpanTag;
 import org.springframework.data.domain.Page;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @RestController
 public class EntryController {
 
-    private final EntryMapper entryMapper;
+	private final EntryMapper entryMapper;
 
-    public EntryController(EntryMapper entryMapper) {
-        this.entryMapper = entryMapper;
-    }
+	public EntryController(EntryMapper entryMapper) {
+		this.entryMapper = entryMapper;
+	}
 
-    @MessageMapping("entries.stream")
-    @NewSpan
-    public Flux<Entry> streamAllEntry(EntryRequest request) {
-        return this.entryMapper.findAll(request.toCriteria(), request.toPageable());
-    }
+	@MessageMapping("entries.stream")
+	public Flux<Entry> streamAllEntry(EntryRequest request) {
+		return this.entryMapper.findAll(request.toCriteria(), request.toPageable());
+	}
 
-    @MessageMapping("entries")
-    @NewSpan
-    public Mono<Page<Entry>> responsePage(EntryRequest request) {
-        return this.entryMapper.findPage(request.toCriteria(), request.toPageable());
-    }
+	@MessageMapping("entries")
+	public Mono<Page<Entry>> responsePage(EntryRequest request) {
+		return this.entryMapper.findPage(request.toCriteria(), request.toPageable());
+	}
 
-    @MessageMapping("entries+c")
-    @NewSpan
-    public Mono<Page<Entry>> responsePageContentIncluded(EntryRequest request) {
-        return this.entryMapper.findPage(request.toCriteria(false), request.toPageable());
-    }
+	@MessageMapping("entries+c")
+	public Mono<Page<Entry>> responsePageContentIncluded(EntryRequest request) {
+		return this.entryMapper.findPage(request.toCriteria(false), request.toPageable());
+	}
 
-    @MessageMapping("entries.{entryId}")
-    @NewSpan
-    public Mono<Entry> responseEntry(@SpanTag("entryId") @DestinationVariable("entryId") Long entryId) {
-        return this.entryMapper.findOne(entryId, false)
-            .switchIfEmpty(errorResponse(entryId));
-    }
+	@MessageMapping("entries.{entryId}")
+	public Mono<Entry> responseEntry(@SpanTag("entryId") @DestinationVariable("entryId") Long entryId) {
+		return this.entryMapper.findOne(entryId, false)
+				.switchIfEmpty(errorResponse(entryId));
+	}
 
-    static <T> Mono<T> errorResponse(Long entryId) {
-        return Mono.error(() -> new ApplicationErrorException(String.format("The requested entry is not found (entryId = %d)", entryId)));
-    }
+	static <T> Mono<T> errorResponse(Long entryId) {
+		return Mono.error(() -> new ApplicationErrorException(String.format("The requested entry is not found (entryId = %d)", entryId)));
+	}
 }
