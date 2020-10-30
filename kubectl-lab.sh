8e68f55d07a89c0b0618aaa5dd4ff147d80e65a9
Toshiaki Maki <tmaki@pivotal.io>
2020-10-30 22:18:07 +0900

    Add manifests

---
+++ kubectl-lab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+cmd=$1
+shift
+
+ytt \
+  -f k8s/namespace.yml \
+  -f k8s/certificate.yml \
+  -f k8s/app.yml \
+  -f k8s/httpproxy.yml \
+  -f k8s/pdb.yml \
+  -f k8s/secret-config.yml \
+  -f k8s/secret-db.yml \
+  -f k8s/overlays/scale-to-one.yml \
+  -f k8s/overlays/maki-lol.yml \
+  | kbld -f - | kubectl ${cmd} -f - $@
