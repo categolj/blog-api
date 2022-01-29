e40f4f3db93eeb7f6094fcd30eb1304009af13a7
Toshiaki Maki <tmaki@vmware.com>
2022-01-29 23:25:21 +0900

    Configure timeout

--- src/main/java/am/ik/blog/config/R2dbcConfig.java
+++ src/main/java/am/ik/blog/config/R2dbcConfig.java
@@ -1,17 +1,23 @@
 package am.ik.blog.config;
 
+import java.time.Duration;
+
 import io.r2dbc.spi.ConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryOptionsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.Resource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+import static io.r2dbc.postgresql.PostgresqlConnectionFactoryProvider.STATEMENT_TIMEOUT;
+import static io.r2dbc.spi.ConnectionFactoryOptions.CONNECT_TIMEOUT;
 
 @Configuration
 public class R2dbcConfig {
@@ -32,5 +38,12 @@ public class R2dbcConfig {
 				log.warn("Database initializer filed: " + e);
 			}
 		};
+	}
+
+	@Bean
+	public ConnectionFactoryOptionsBuilderCustomizer connectionFactoryOptionsBuilderCustomizer() {
+		return builder -> builder
+				.option(CONNECT_TIMEOUT, Duration.ofSeconds(10))
+				.option(STATEMENT_TIMEOUT, Duration.ofSeconds(10));
 	}
 }
