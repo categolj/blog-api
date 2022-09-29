907b39fe9e2c6927064f2be5514d89db04faae16
Toshiaki Maki <tmaki@vmware.com>
2022-09-29 15:29:54 +0900

    Add Cors Mappings

--- src/main/java/am/ik/blog/config/WebConfig.java
+++ src/main/java/am/ik/blog/config/WebConfig.java
@@ -3,9 +3,22 @@ package am.ik.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST")
+				.maxAge(3600);
+	}
+
 	@Bean
 	public ReactivePageableHandlerMethodArgumentResolver pageableArgumentResolver() {
 		return new ReactivePageableHandlerMethodArgumentResolver();
