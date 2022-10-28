fe68f95e6a3838cc7837aa53fe1c4808b2aff28f
Toshiaki Maki <tmaki@vmware.com>
2022-10-29 05:21:35 +0900

    Add CommonsRequestLoggingFilter

--- src/main/java/am/ik/blog/config/WebConfig.java
+++ src/main/java/am/ik/blog/config/WebConfig.java
@@ -1,11 +1,30 @@
 package am.ik.blog.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		final CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter() {
+			@Override
+			protected boolean shouldLog(HttpServletRequest request) {
+				final String uri = request.getRequestURI();
+				final boolean deny = uri != null && (uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator/health"));
+				return !deny;
+			}
+		};
+		filter.setIncludeQueryString(true);
+		filter.setIncludeHeaders(true);
+		filter.setIncludeClientInfo(true);
+		return filter;
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
