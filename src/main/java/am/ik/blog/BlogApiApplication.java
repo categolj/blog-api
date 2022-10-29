f79dda4428a533f946a948352fc28c78966a2e4e
Toshiaki Maki <tmaki@vmware.com>
2022-10-29 15:47:13 +0900

    Remove unused hook

--- src/main/java/am/ik/blog/BlogApiApplication.java
+++ src/main/java/am/ik/blog/BlogApiApplication.java
@@ -2,8 +2,6 @@ package am.ik.blog;
 
 import java.time.Instant;
 
-import reactor.core.publisher.Hooks;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
-		Hooks.onErrorDropped(e -> { /* https://github.com/rsocket/rsocket-java/issues/1018 */});
 		System.setProperty("info.env.launch", Instant.now().toString());
 		SpringApplication.run(BlogApiApplication.class, args);
 	}
