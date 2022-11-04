5d1df8bd59f3ec6f8d20a365b0331c70054d28ac
Toshiaki Maki <tmaki@vmware.com>
2022-11-04 11:45:24 +0900

    Exclude all actuator endpoints

--- src/main/java/am/ik/blog/config/UriFilter.java
+++ src/main/java/am/ik/blog/config/UriFilter.java
@@ -6,7 +6,7 @@ public class UriFilter implements Predicate<String> {
 
 	@Override
 	public boolean test(String uri) {
-		final boolean deny = uri != null && (uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator/health") || uri.startsWith("/_static"));
+		final boolean deny = uri != null && (uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator") || uri.startsWith("/_static"));
 		return !deny;
 	}
 }
