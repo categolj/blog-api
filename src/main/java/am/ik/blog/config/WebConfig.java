a256cf19ac27445ffc38d20adf12e76db5362628
Toshiaki Maki <tmaki@vmware.com>
2022-11-07 20:03:19 +0900

    Allow options

--- src/main/java/am/ik/blog/config/WebConfig.java
+++ src/main/java/am/ik/blog/config/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedOrigins("*")
-				.allowedMethods("GET", "POST")
+				.allowedMethods("GET", "POST", "OPTIONS")
 				.maxAge(3600);
 	}
 }
