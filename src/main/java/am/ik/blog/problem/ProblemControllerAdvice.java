0b190a4a88a67b958880eb0992f55c8705a95830
Toshiaki Maki <tmaki@vmware.com>
2022-11-25 03:41:01 +0900

    Handle runtime exception

--- src/main/java/am/ik/blog/problem/ProblemControllerAdvice.java
+++ src/main/java/am/ik/blog/problem/ProblemControllerAdvice.java
@@ -1,6 +1,10 @@
 package am.ik.blog.problem;
 
 import am.ik.yavi.core.ConstraintViolationsException;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -11,6 +15,14 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class ProblemControllerAdvice {
+	private final Logger log = LoggerFactory.getLogger(ProblemControllerAdvice.class);
+
+	private final Tracer tracer;
+
+	public ProblemControllerAdvice(Tracer tracer) {
+		this.tracer = tracer;
+	}
+
 	@ExceptionHandler(ResponseStatusException.class)
 	public ProblemDetail handleResponseStatusException(ResponseStatusException e) {
 		final ProblemDetail problemDetail = ProblemDetail.forStatus(e.getStatusCode());
@@ -23,6 +35,18 @@ public class ProblemControllerAdvice {
 	public ProblemDetail handleConstraintViolationsException(ConstraintViolationsException e) {
 		final ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Constraint violations found!");
 		problemDetail.setProperty("violations", e.violations().details());
+		return problemDetail;
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ProblemDetail handleRuntimeException(RuntimeException e) {
+		final ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred!");
+		log.error("Unexpected error occurred!", e);
+		final Span currentSpan = tracer.currentSpan();
+		if (currentSpan != null) {
+			problemDetail.setProperty("traceId", currentSpan.context().traceId());
+		}
 		return problemDetail;
 	}
 }
