0c43ccfd4b01485d4ffd894811997f1759a8ee88
Toshiaki Maki <tmaki@vmware.com>
2022-11-26 03:39:42 +0900

    Avoid 'not eligible for getting processed by all BeanPostProcessors' messages

--- src/main/java/am/ik/blog/config/OtelConfig.java
+++ src/main/java/am/ik/blog/config/OtelConfig.java
@@ -3,6 +3,7 @@ package am.ik.blog.config;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +11,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 public class OtelConfig {
 	@Bean
-	public BeanPostProcessor filteringSpanExporterRegistrar(UriFilter uriFilter) {
+	public static BeanPostProcessor filteringSpanExporterRegistrar(ObjectProvider<UriFilter> uriFilter) {
 		return new BeanPostProcessor() {
 			@Override
 			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 				if (bean instanceof final SpanExporter spanExporter) {
-					return new FilteringSpanExporter(spanExporter, uriFilter);
+					return new FilteringSpanExporter(spanExporter, uriFilter.getObject());
 				}
 				return bean;
 			}
