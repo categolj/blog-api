a3279ea15c119a389cd1d8d7fa513a23650f613f
Toshiaki Maki <makingx@gmail.com>
2023-08-24 11:22:00 +0900

    Set default tenantId

--- src/main/resources/am/ik/blog/entry/EntryMapper/findAllCursorNext.sql
+++ src/main/resources/am/ik/blog/entry/EntryMapper/findAllCursorNext.sql
@@ -1,41 +1,43 @@
 SELECT e.entry_id,
-              e.title,
+       e.title,
 /*[# th:if="!${excludeContent}"]*/
-              e.content,
+       e.content,
 /*[/]*/
 /*[# th:if="${excludeContent}"][# th:utext="|       '' AS content,|"][/][/]*/
-              COALESCE(e.categories, '{}') AS categories,
-              COALESCE(e.tags, '{}')       AS tags,
-              e.created_by,
-              e.created_date,
-              e.last_modified_by,
-              e.last_modified_date
-       FROM entry AS e
-       WHERE e.last_modified_date < COALESCE( /*[# mb:p="cursor"]*/ NULL /*[/]*/ , 'infinity'::timestamptz)
+       COALESCE(e.categories, '{}') AS categories,
+       COALESCE(e.tags, '{}')       AS tags,
+       e.created_by,
+       e.created_date,
+       e.last_modified_by,
+       e.last_modified_date
+FROM entry AS e
+WHERE e.last_modified_date < COALESCE( /*[# mb:p="cursor"]*/ NULL /*[/]*/ , 'infinity'::timestamptz)
 /*[# th:if="${keywordsCount > 0}"]*/
 /*[# th:each="i : ${#numbers.sequence(0, keywordsCount - 1)}"]*/
-         AND e.keywords @> ARRAY[ /*[# mb:p="keywords[${i}]"]*/ 'JAVA' /*[/]*/ ]::character varying[]
+  AND e.keywords @> ARRAY [ /*[# mb:p="keywords[${i}]"]*/ 'JAVA' /*[/]*/ ]::character varying[]
 /*[/]*/
 /*[/]*/
 /*[# th:if="${createdBy} != null"]*/
-         AND e.created_by = /*[# mb:p="createdBy"]*/ 'Toshiaki Maki' /*[/]*/
+  AND e.created_by = /*[# mb:p="createdBy"]*/ 'Toshiaki Maki' /*[/]*/
 /*[/]*/
 /*[# th:if="${lastModifiedBy} != null"]*/
-         AND e.last_modified_by = /*[# mb:p="lastModifiedBy"]*/ 'Toshiaki Maki' /*[/]*/
+  AND e.last_modified_by = /*[# mb:p="lastModifiedBy"]*/ 'Toshiaki Maki' /*[/]*/
 /*[/]*/
 /*[# th:if="${not #lists.isEmpty(categories)}"]*/
 /*[# th:each="category,stat : ${categories}"]*/
-         AND e.categories/*[# th:utext="${'[' + stat.count + ']'}"]*/ [1] /*[/]*/ = /*[# mb:p="categories[${stat.index}]"]*/ 'Java' /*[/]*/
+  AND e.categories/*[# th:utext="${'[' + stat.count + ']'}"]*/ [1] /*[/]*/ = /*[# mb:p="categories[${stat.index}]"]*/
+      'Java' /*[/]*/
 /*[/]*/
 /*[/]*/
 /*[# th:if="${tag} != null"]*/
-         AND e.tags @> ARRAY[ /*[# mb:p="tag"]*/ 'Java' /*[/]*/ ]::character varying[]
+  AND e.tags @> ARRAY [ /*[# mb:p="tag"]*/ 'Java' /*[/]*/ ]::character varying[]
 /*[/]*/
 /*[# th:if="${tenantId}"]*/
-         AND e.tenant_id = /*[# mb:p="tenantId"]*/ '_'
+  AND e.tenant_id = /*[# mb:p="tenantId"]*/ '_'
 /*[/]*/
 /*[/]*/
-       ORDER BY e.last_modified_date DESC
+/*[# th:unless="${tenantId}"][# th:utext="|  AND e.tenant_id = '_'|"][/][/]*/
+ORDER BY e.last_modified_date DESC
 /*[# th:if="${limit}"]*/
-       LIMIT 20 OFFSET 0
+LIMIT 20 OFFSET 0
 /*[/]*/
