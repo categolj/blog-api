13143f16f8764e33a4f9d4c85e00c89cdedaa89a
Toshiaki Maki <makingx@gmail.com>
2023-08-24 12:03:19 +0900

    Enable VirtualThreads

---
+++ src/main/java/am/ik/blog/config/VirtualThreadsConfig.java
@@ -0,0 +1,23 @@
+package am.ik.blog.config;
+
+import org.springframework.boot.autoconfigure.web.embedded.TomcatVirtualThreadsWebServerFactoryCustomizer;
+import org.springframework.boot.system.JavaVersion;
+import org.springframework.boot.web.embedded.tomcat.ConfigurableTomcatWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Remove when updating to Java 21
+@Configuration(proxyBeanMethods = false)
+public class VirtualThreadsConfig {
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableTomcatWebServerFactory> tomcatVirtualThreadsProtocolHandlerCustomizer() {
+		if (JavaVersion.getJavaVersion().equals(JavaVersion.TWENTY)) {
+			return new TomcatVirtualThreadsWebServerFactoryCustomizer();
+		}
+		else {
+			return factory -> {
+			};
+		}
+	}
+}
