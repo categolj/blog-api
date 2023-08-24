097a485f2c9909aa89b21e2617c31982a9fb6c28
Toshiaki Maki <makingx@gmail.com>
2023-08-24 22:17:24 +0900

    Remove gson from flyway

---
+++ src/main/java/org/flywaydb/core/internal/reports/json/MigrateResultDeserializer.java
@@ -0,0 +1,10 @@
+package org.flywaydb.core.internal.reports.json;
+
+import org.flywaydb.core.extensibility.Plugin;
+
+public class MigrateResultDeserializer implements Plugin {
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+}
