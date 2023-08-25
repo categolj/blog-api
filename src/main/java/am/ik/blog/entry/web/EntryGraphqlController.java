0095db666787fc530a66d84c0cff219907063fa7
Toshiaki Maki <makingx@gmail.com>
2023-08-25 20:12:56 +0900

    Add Cursor-based pagination

--- src/main/java/am/ik/blog/entry/web/EntryGraphqlController.java
+++ src/main/java/am/ik/blog/entry/web/EntryGraphqlController.java
@@ -1,14 +1,21 @@
 package am.ik.blog.entry.web;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 import am.ik.blog.entry.Entry;
 import am.ik.blog.entry.EntryService;
+import am.ik.blog.entry.search.SearchCriteria;
+import am.ik.pagination.CursorPage;
+import am.ik.pagination.CursorPageRequest;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import graphql.schema.DataFetchingFieldSelectionSet;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 
 @Controller
 public class EntryGraphqlController {
@@ -29,5 +36,41 @@ public class EntryGraphqlController {
 			@Argument String tenantId, DataFetchingFieldSelectionSet selectionSet) {
 		final boolean excludeContent = !selectionSet.contains("content");
 		return this.entryService.findOne(entryId, tenantId, excludeContent);
+	}
+
+	@QueryMapping
+	public EntryConnection getEntries(@Argument Integer first, @Argument String after) {
+		final CursorPageRequest<Instant> pageRequest = new CursorPageRequest<>(
+				StringUtils.hasText(after) ? Instant.parse(after) : null, first,
+				CursorPageRequest.Navigation.NEXT);
+		final CursorPage<Entry, Instant> page = this.entryService
+				.findPage(SearchCriteria.DEFAULT, null, pageRequest);
+		final List<EntryEdge> edges = page.content().stream().map(EntryEdge::new)
+				.toList();
+		PageInfo pageInfo;
+		if (edges.isEmpty()) {
+			pageInfo = new PageInfo(null, null, page.hasNext(), page.hasPrevious());
+		}
+		else {
+			pageInfo = new PageInfo(edges.get(0).cursor(),
+					edges.get(edges.size() - 1).cursor(), page.hasNext(),
+					page.hasPrevious());
+		}
+		return new EntryConnection(edges, pageInfo);
+	}
+
+	public record EntryEdge(Entry node) {
+		@JsonProperty
+		public String cursor() {
+			return node.getUpdated().getDate().toString();
+		}
+	}
+
+	public record PageInfo(String startCursor, String endCursor, boolean hasNextPage,
+			boolean hadPreviousPage) {
+
+	}
+
+	public record EntryConnection(List<EntryEdge> edges, PageInfo pageInfo) {
 	}
 }
