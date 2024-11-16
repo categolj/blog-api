3b031f332ce357b215fee6ce7ed513441359f25a
Toshiaki Maki <makingx@gmail.com>
2024-11-17 08:43:16 +0900

    Polish native hints

--- src/main/java/am/ik/blog/config/NativeHints.java
+++ src/main/java/am/ik/blog/config/NativeHints.java
@@ -4,19 +4,24 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import am.ik.blog.config.NativeHints.RuntimeHints;
 import am.ik.blog.entry.EntryBuilder;
 import am.ik.blog.entry.FrontMatterBuilder;
+import am.ik.blog.tenant.TenantUserProps;
 import jakarta.annotation.Nullable;
 
+import org.apache.tomcat.util.modeler.modules.MbeansDescriptorsIntrospectionSource;
 import org.flywaydb.core.internal.configuration.extensions.DeployScriptFilenameConfigurationExtension;
 import org.flywaydb.core.internal.configuration.extensions.PrepareScriptFilenameConfigurationExtension;
+import org.flywaydb.core.internal.logging.slf4j.Slf4jLogCreator;
 import org.flywaydb.core.internal.publishing.PublishingConfigurationExtension;
 import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.util.ReflectionUtils;
 
 @Configuration(proxyBeanMethods = false)
 @ImportRuntimeHints(RuntimeHints.class)
@@ -35,16 +40,13 @@ public class NativeHints {
 				builderMethods
 					.addAll(Arrays.asList(PrepareScriptFilenameConfigurationExtension.class.getDeclaredMethods()));
 				builderMethods.addAll(Arrays.asList(PublishingConfigurationExtension.class.getDeclaredMethods()));
-				builderMethods.stream()
-					.forEach(method -> hints.reflection().registerMethod(method, ExecutableMode.INVOKE));
+				builderMethods.forEach(method -> hints.reflection().registerMethod(method, ExecutableMode.INVOKE));
+				hints.reflection().registerConstructor(Slf4jLogCreator.class.getConstructor(), ExecutableMode.INVOKE);
 				hints.reflection()
-					.registerConstructor(
-							org.flywaydb.core.internal.logging.slf4j.Slf4jLogCreator.class.getConstructor(),
+					.registerConstructor(MbeansDescriptorsIntrospectionSource.class.getConstructor(),
 							ExecutableMode.INVOKE);
 				hints.reflection()
-					.registerConstructor(
-							org.apache.tomcat.util.modeler.modules.MbeansDescriptorsIntrospectionSource.class
-								.getConstructor(),
+					.registerMethod(Objects.requireNonNull(ReflectionUtils.findMethod(TenantUserProps.class, "users")),
 							ExecutableMode.INVOKE);
 			}
 			catch (Exception e) {
