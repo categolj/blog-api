17d51e8408df9b55193b3fd0b3bdc07a9dc2e617
Toshiaki Maki <makingx@gmail.com>
2024-11-16 22:10:45 +0900

    Apply workarounds to build a native image

--- src/main/java/am/ik/blog/config/NativeHints.java
+++ src/main/java/am/ik/blog/config/NativeHints.java
@@ -4,19 +4,19 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 import am.ik.blog.config.NativeHints.RuntimeHints;
 import am.ik.blog.entry.EntryBuilder;
 import am.ik.blog.entry.FrontMatterBuilder;
-import am.ik.blog.tenant.TenantUserProps;
 import jakarta.annotation.Nullable;
 
+import org.flywaydb.core.internal.configuration.extensions.DeployScriptFilenameConfigurationExtension;
+import org.flywaydb.core.internal.configuration.extensions.PrepareScriptFilenameConfigurationExtension;
+import org.flywaydb.core.internal.publishing.PublishingConfigurationExtension;
 import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.util.ReflectionUtils;
 
 @Configuration(proxyBeanMethods = false)
 @ImportRuntimeHints(RuntimeHints.class)
@@ -30,8 +30,12 @@ public class NativeHints {
 				final List<Method> builderMethods = new ArrayList<>();
 				builderMethods.addAll(Arrays.asList(EntryBuilder.class.getDeclaredMethods()));
 				builderMethods.addAll(Arrays.asList(FrontMatterBuilder.class.getDeclaredMethods()));
+				builderMethods
+					.addAll(Arrays.asList(DeployScriptFilenameConfigurationExtension.class.getDeclaredMethods()));
+				builderMethods
+					.addAll(Arrays.asList(PrepareScriptFilenameConfigurationExtension.class.getDeclaredMethods()));
+				builderMethods.addAll(Arrays.asList(PublishingConfigurationExtension.class.getDeclaredMethods()));
 				builderMethods.stream()
-					.filter(m -> m.getName().equals("build") || m.getName().startsWith("with"))
 					.forEach(method -> hints.reflection().registerMethod(method, ExecutableMode.INVOKE));
 				hints.reflection()
 					.registerConstructor(
@@ -41,15 +45,6 @@ public class NativeHints {
 					.registerConstructor(
 							org.apache.tomcat.util.modeler.modules.MbeansDescriptorsIntrospectionSource.class
 								.getConstructor(),
-							ExecutableMode.INVOKE);
-				// https://github.com/oracle/graal/issues/5626
-				hints.reflection()
-					.registerConstructor(org.hibernate.validator.internal.util.logging.Log_$logger.class
-						.getConstructor(org.jboss.logging.Logger.class), ExecutableMode.INVOKE)
-					.registerField(
-							org.hibernate.validator.internal.util.logging.Messages_$bundle.class.getField("INSTANCE"));
-				hints.reflection()
-					.registerMethod(Objects.requireNonNull(ReflectionUtils.findMethod(TenantUserProps.class, "users")),
 							ExecutableMode.INVOKE);
 			}
 			catch (Exception e) {
